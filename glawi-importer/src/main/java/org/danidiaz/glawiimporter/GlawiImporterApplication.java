c114315b03b64156da4b82eaa23f629271497fcb
Daniel Diaz <diaz.carrete@facebook.com>
2020-07-30 13:57:39 +0200

    glawi-importer

---
+++ glawi-importer/src/main/java/org/danidiaz/glawiimporter/GlawiImporterApplication.java
@@ -0,0 +1,13 @@
+package org.danidiaz.glawiimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlawiImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlawiImporterApplication.class, args);
+	}
+
+}
---
+++ glawi-importer/src/test/java/org/danidiaz/glawiimporter/GlawiImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.danidiaz.glawiimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlawiImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
